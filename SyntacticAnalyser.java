c6abcd4cc750ea65621d4b4bd4ba5a5797f1e761
geewik <guy.wikramanayake@gmail.com>
2024-09-23 12:41:47 +1000

    Initial commit

--- SyntacticAnalyser.java
+++ SyntacticAnalyser.java
@@ -8,7 +8,30 @@ import java.util.Map;
 public class SyntacticAnalyser {
 
 	public static ParseTree parse(List<Token> tokens) throws SyntaxException {
-		//Turn the List of Tokens into a ParseTree.
+
+		var tree = new ParseTree();
+		var stack = new ArrayDeque<Pair<Symbol, TreeNode>>();
+
+		stack.push(new Pair<> (TreeNode.Label.prog,null));
+
+		var position = 0;
+
+		while (!stack.isEmpty() && position < tokens.size()) {
+			var s = stack.pop();
+			var newNode = new TreeNode(getLabel(s.fst()), tokens.get(position), s.snd());
+			applyRule(s.fst(), tokens.get(position), stack, newNode);
+
+			if (s.snd() == null){
+				tree.setRoot(newNode);
+			} else {
+				s.snd().addChild(newNode);
+			}
+
+			if (!s.fst().isVariable()){
+				position ++;
+			}
+		}
+
 		return new ParseTree();
 	}
 
