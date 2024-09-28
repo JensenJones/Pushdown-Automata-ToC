f7724705dd4e1023dbef7e37aaadb2388941bf9e
geewik <guy.wikramanayake@gmail.com>
2024-09-28 17:56:59 +1000

    Changed valueOf to terminal in parsingtable

--- ParsingTable.java
+++ ParsingTable.java
@@ -8,21 +8,21 @@ class ParsingTable {
 
     public ParsingTable() {
         TreeNode progNode = new TreeNode(TreeNode.Label.prog, null);
-        List<TreeNode> progRuleA = Arrays.asList(new TreeNode(TreeNode.Label.valueOf("public"), new Token(Token.TokenType.PUBLIC), progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("class"), new Token(Token.TokenType.CLASS) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("ID"), new Token(Token.TokenType.ID) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("{"), new Token(Token.TokenType.LBRACE) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("public"), new Token(Token.TokenType.PUBLIC), progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("static"), new Token(Token.TokenType.STATIC) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("void"), new Token(Token.TokenType.VOID) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("main"), new Token(Token.TokenType.MAIN) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("("), new Token(Token.TokenType.LPAREN) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("string[]"), new Token(Token.TokenType.STRINGARR) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("args"), new Token(Token.TokenType.ARGS) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("{"), new Token(Token.TokenType.LBRACE) ,progNode),
+        List<TreeNode> progRuleA = Arrays.asList(new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.PUBLIC), progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.CLASS), progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.ID) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LBRACE) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.PUBLIC), progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.STATIC) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.VOID) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.MAIN) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LPAREN) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.STRINGARR) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.ARGS) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LBRACE) ,progNode),
                                                  new TreeNode(TreeNode.Label.los, progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("}"), new Token(Token.TokenType.RBRACE) ,progNode),
-                                                 new TreeNode(TreeNode.Label.valueOf("}"), new Token(Token.TokenType.RBRACE) ,progNode));
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.RBRACE) ,progNode),
+                                                 new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.RBRACE) ,progNode));
         parsingTable.put(new Pair<>(progNode, new Token(Token.TokenType.PUBLIC)), progRuleA);
     }
 
