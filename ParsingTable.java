887199f4e391d9f34d78402a4b4a0c55e80f60bb
geewik <guy.wikramanayake@gmail.com>
2024-09-28 19:33:08 +1000

    creating pairs

--- ParsingTable.java
+++ ParsingTable.java
@@ -1,39 +1,32 @@
 import java.util.*;
 
 class ParsingTable {
-    private final Map<Pair<TreeNode, Token>, List<TreeNode>> parsingTable = new HashMap<>();
+    private final Map<Pair<Symbol, Token.TokenType>, List<Pair<Symbol, Token.TokenType>>> parsingTable = new HashMap<>();
 
     // Key:
     // Value:
 
     public ParsingTable() {
-        TreeNode progNode = new TreeNode(TreeNode.Label.prog, null);
-
-
-        List<TreeNode> progRuleA = Arrays.asList(
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.PUBLIC), null),  // Pass `null` as the parent
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.CLASS), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.ID), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LBRACE), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.PUBLIC), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.STATIC), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.VOID), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.MAIN), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LPAREN), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.STRINGARR), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.ARGS), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.LBRACE), null),
-                new TreeNode(TreeNode.Label.los, null),  // Pass `null` as the parent
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.RBRACE), null),
-                new TreeNode(TreeNode.Label.terminal, new Token(Token.TokenType.RBRACE), null)
+        List<Pair<Symbol, Token.TokenType>> progRuleA = Arrays.asList(
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.PUBLIC),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.CLASS),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.ID),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.LBRACE),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.PUBLIC),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.STATIC),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.VOID),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.MAIN),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.LPAREN),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.STRINGARR),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.ARGS),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.LBRACE),
+                new Pair<>(TreeNode.Label.los, null),  // Non-terminal with no TokenType
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.RBRACE),
+                new Pair<>(TreeNode.Label.terminal, Token.TokenType.RBRACE)
         );
-        parsingTable.put(new Pair<>(progNode, new Token(Token.TokenType.PUBLIC)), progRuleA);
     }
 
-    /*
-     * Returns the production rule or null
-     */
-    public List<TreeNode> applyRule(Pair<TreeNode, Token> key) {
+    public List<Pair<Symbol, Token.TokenType>> applyRule(Pair<Symbol, Token.TokenType> key) {
         return parsingTable.get(key);
     }
 }
